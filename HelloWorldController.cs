d7b75ef64725042a16051bfc48aa69e0aff3701b
Noelia Camacho <noeliacamacho.m6@gmail.com>
2025-04-10 16:56:15 -0300

    Add files via upload

--- HelloWorldController.cs
+++ HelloWorldController.cs
@@ -1,26 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
+
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/ Reemplazar el método index:
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
-        }
-
-        // 
-        // GET: /HelloWorld/Welcome/ Agregar este método:
-        public string Welcome(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+            return View();
         }
 
 
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
+
+            return View();
+        }
     }
 }
